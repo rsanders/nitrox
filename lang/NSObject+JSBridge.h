a38816f8173cb94b0a88698ce4e7fcf134de2aac
Robert Sanders <robert@vitrue.com>
2008-10-01 18:20:24 -0400

    simple utility class to list methods of a class

---
+++ lang/NSObject+JSBridge.h
@@ -0,0 +1,17 @@
+//
+//  NSObject+JSBridge.h
+//  nitrox
+//
+//  Created by Robert Sanders on 10/1/08.
+//  Copyright 2008 ViTrue, Inc.. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface NSObject (JSBridge)
+
+- (NSArray*) instanceMethodNames;
+- (NSArray*) classMethodNames;
+
+@end
---
+++ lang/NSObject+JSBridge.m
@@ -0,0 +1,37 @@
+//
+//  NSObject+Reflection.m
+//  nitroxy1
+//
+//  Created by Robert Sanders on 10/1/08.
+//  Copyright 2008 ViTrue, Inc.. All rights reserved.
+//
+
+#import "NSObject+JSBridge.h"
+#import <objc/objc-runtime.h>
+
+@implementation NSObject (JSBridge)
+
+- (NSArray*) methodNamesForClass:(Class)clazz {
+    unsigned int count;
+    Method * methodList = class_copyMethodList(clazz, &count);    
+    NSMutableArray *names = [[NSMutableArray alloc] init];
+
+    for (int i = 0; i < count; i++) {
+        Method method = methodList[i];
+        [names addObject:[NSString stringWithCString:sel_getName(method_getName(method))]];
+    }
+    
+    free(methodList);
+    
+    return [names autorelease];
+}
+
+- (NSArray*) instanceMethodNames {
+    return [self methodNamesForClass:[self class]];
+}
+
+- (NSArray*) classMethodNames {
+    return [self methodNamesForClass:object_getClass([self class])];
+}
+
+@end
