d04ba0d37d547ed1274870f8b320236fafd29acb
Robert Sanders <robert@vitrue.com>
2008-10-05 16:44:47 -0400

    primitive wrapper for location, accel, and vibration functions

--- web/lib/nitrox.js
+++ web/lib/nitrox.js
@@ -57,25 +57,25 @@ Nitrox.Runtime = {
 
 Nitrox.Bridge = {
     'call': function(fun, args, async) {
-        Nitrox.log("step 1");
+            Nitrox.log("step 1");
             Nitrox.log('FOO2 starting bridge call for ' + fun);
             var id = "id" + i++;
             if (!async) async = false;
-        Nitrox.log("step 2, id="+id);
+            Nitrox.log("step 2, id="+id);
             Nitrox.log('FOO starting bridgecall for id ' + id);
             var port = Nitrox.Runtime.port;
             // clone args
-        Nitrox.log("step 3");
+            Nitrox.log("step 3");
             args = jQuery.extend(true, args, {'id': id, 'token': Nitrox.Runtime.token});
             var fullstring = "http://localhost:" + port + "/rpc/" + fun;
-        Nitrox.log("Step 4, url=" + fullstring);
+            Nitrox.log("Step 4, url=" + fullstring);
             var req;
             try {
                 req = jQuery.ajax({url: fullstring, data: args, async: async, type: 'get'});
             } catch (e) {
                 req = {error: e, status:401, responseText: "Error: " + e};
             }
-        Nitrox.log("step 5");
+            Nitrox.log("step 5");
             if (!req) {
                 Nitrox.log("No request object returned");
                 req = {error: "unknown", status:500, responseText:"No req object returned"};
@@ -86,11 +86,57 @@ Nitrox.Bridge = {
             } else {
                 Nitrox.log('error code: ' + req.status);
             }
-        Nitrox.log('returning from id=' + id);
+            Nitrox.log('returning from id=' + id);
             return id;
         },
 
     'version': '0.1'
+};
+
+Nitrox.Location = {
+    start: function(async) {
+        Nitrox.Bridge.call('Location/c/start', {}, async);
+    },
+
+    stop: function(async) {
+        Nitrox.Bridge.call('Location/c/stop', {}, async);
+    },
+    
+    getLocation: function() {
+        var location = Nitrox.Bridge.call('Location/c/getLocation', {}, true);
+        Nitrox.log("location is " + location);
+        return location;
+    },
+    
+    version: '0.1'
+};
+
+Nitrox.Accelerometer = {
+    start: function(async) {
+        Nitrox.Bridge.call('Accelerometer/c/start', {}, async);
+    },
+
+    stop: function(async) {
+        Nitrox.Bridge.call('Accelerometer/c/stop', {}, async);
+    },
+    
+    getAcceleration: function() {
+        var location = Nitrox.Bridge.call('Accelerometer/c/getAcceleration', {}, true);
+        Nitrox.log("acceleration is " + location);
+        return location;
+    },
+    
+    version: '0.1'
+};
+
+
+
+Nitrox.Vibrate = {
+    vibrate: function() {
+        Nitrox.Bridge.call('Vibrate/c/vibrate', {}, true);
+    },
+    
+    version: '0.1'
 };
 
 Nitrox.File = function(path) {
