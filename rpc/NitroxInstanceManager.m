b86bfff59abfc7c9effb9822bc7c9c091749e869
Robert Sanders <robert@vitrue.com>
2008-10-06 12:20:45 -0400

    skeleton for distributed instance manager

---
+++ rpc/NitroxInstanceManager.m
@@ -0,0 +1,14 @@
+//
+//  NitroxInstanceManager.m
+//  nitroxy1
+//
+//  Created by Robert Sanders on 10/3/08.
+//  Copyright 2008 ViTrue, Inc.. All rights reserved.
+//
+
+#import "NitroxInstanceManager.h"
+
+
+@implementation NitroxInstanceManager
+
+@end
