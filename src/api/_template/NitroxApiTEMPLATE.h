bea810ff195c09e7b56ac055f6912d62fbf967ac
Robert Sanders <robert@vitrue.com>
2008-10-17 13:25:25 -0400

    API module template

---
+++ src/api/_template/NitroxApiTEMPLATE.h
@@ -0,0 +1,19 @@
+//
+//  NitroxApiPhoto.h
+//
+//  Created by Robert Sanders on 10/3/08.
+//  Copyright 2008 ViTrue, Inc.. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NitroxRPC.h"
+
+@interface NitroxApiTEMPLATE : NitroxStubClass {
+
+}
+
+- (NitroxApiTEMPLATE *)init;
+
+
+@end
---
+++ src/api/_template/NitroxApiTEMPLATE.m
@@ -0,0 +1,32 @@
+//
+//  NitroxApiTEMPLATE.m
+//
+//  Created by Robert Sanders on 10/3/08.
+//  Copyright 2008 ViTrue, Inc.. All rights reserved.
+//
+
+#import "NitroxApiTEMPLATE.h"
+
+
+@implementation NitroxApiTEMPLATE
+
+- (NitroxApiTEMPLATE *)init
+{
+    [super init];
+
+    return self;
+}
+
+- (void) dealloc {
+    [super dealloc];
+}
+
+#pragma mark API specific methods
+
+
+#pragma mark delegate and notification methods
+
+#pragma mark support methods
+
+
+@end
